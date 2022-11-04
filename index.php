688589d93913e0adb4dfc7c80969cdcb5ad77f14
projetodoin <projetodoin@gmail.com>
2022-11-04 08:38:28 -0300

    save

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Principal</title>
+    <link rel="stylesheet" href="estilo_index.css">
+    
+</head>
+<body>
+    <h1>BEM VINDO AO DOIN</h1>
+    <div id="centro">
+        <ul>
+            <li><a href="cadastro_produto/cadastro_produto.html">Cadastre seu produto aqui!</li>
+            <li><a href="cadastro_usuario/cadastro_usuario.html">Cadastre-se aqui!</li>
+        </ul>
+    </div>
+</body>
+</html>
